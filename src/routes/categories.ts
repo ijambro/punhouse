47b9b0ba70a725771073d05314090a3c979b06f5
Jake Palmer <jake.palmer@archivesocial.com>
2021-05-24 01:12:20 -0400

    add route to get category by id

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -5,18 +5,31 @@ import categoriesController from "../controllers/categories";
 import Category from "../category";
 
 router.get("/", (req, res) => {
-    // let categoriesController = new CategoriesController();
     res.json(categoriesController.all());
 });
 
 router.get("/all", (req, res) => {
-    // let categoriesController = new CategoriesController();
     res.json(categoriesController.all());
 });
 
 router.get("/random", (req, res) => {
-    // let categoriesController = new CategoriesController();
     res.json(categoriesController.random());
 });
+
+router.get("/:id", (req, res) => {
+    let idNum = Number.parseInt(req.params.id);
+    if (idNum === NaN) {
+        res.sendStatus(400);
+    } else {
+        let c = categoriesController.get(idNum);
+        if (c === undefined) {
+            res.sendStatus(404);
+        } else {
+            res.json(c);
+        }
+    }
+});
+
+
 
 export default router;
